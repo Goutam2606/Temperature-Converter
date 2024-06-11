8cd50f073b3323138cb9ffb43a8044a606199569
Goutam2606 <goutamy.bca2022@ssism.org>
2024-06-11 23:04:47 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'temperature-converter';
+
+  celsius: number = 0;
+  fahrenheit: number = 0;
+
+  convertToCelsius() {
+    this.celsius = (this.fahrenheit - 32) * 5 / 9;
+  }
+  convertToFahrenheit() {
+
+    this.fahrenheit = (this.celsius * 9 / 5) + 32;
+  }
 }
